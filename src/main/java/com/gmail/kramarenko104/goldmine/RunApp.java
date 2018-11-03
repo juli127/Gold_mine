d01ccdccf1cb9b09614d1bfa522cf79641e01a41
juli127 <juli127@bigmir.net>
2018-11-04 00:07:02 +0200

    some small corrections

--- src/main/java/com/gmail/kramarenko104/goldmine/RunApp.java
+++ src/main/java/com/gmail/kramarenko104/goldmine/RunApp.java
@@ -1,24 +1,21 @@
 package com.gmail.kramarenko104.goldmine;
 
-import java.util.Scanner;
-
 public class RunApp {
 
     static final int START_WORKERS_COUNT = 5;
 
-	public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException {
 
-		/*
+/*
 Gold Mine. есть шахта на 1000 золота.
 и есть бараки, которые производят 1 рабочего в 10 сек.
 Рабочий добывает 3 золота / сек. Изначально есть 5 рабочих.
 Программа заканчивается , когда в шахте становиться <= 0 золота.
 Все действия выводить в консоль в понятном и читабельном виде.
-		 */
-		GoldenMine goldenMine = new GoldenMine(1000);
+ */
+	GoldenMine goldenMine = new GoldenMine(1000);
         Barack barack = new Barack(START_WORKERS_COUNT, goldenMine);
 
         new Thread(barack).start();
-
-	}
+    }
 }
